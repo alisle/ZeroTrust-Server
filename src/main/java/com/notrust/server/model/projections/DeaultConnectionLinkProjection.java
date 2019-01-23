143df1496a6ae431b3e86341763ac329c6aec5a8
alisle <alex.lisle@gmail.com>
2019-01-23 11:26:01 -0600

    added default projection to make the connectionlink REST endpoint less of a pain

---
+++ src/main/java/com/notrust/server/model/projections/DeaultConnectionLinkProjection.java
@@ -0,0 +1,18 @@
+package com.notrust.server.model.projections;
+
+import com.notrust.server.model.Agent;
+import com.notrust.server.model.Connection;
+import com.notrust.server.model.ConnectionLink;
+import org.springframework.data.rest.core.config.Projection;
+
+import java.time.Instant;
+
+@Projection(name="DefaultConnectionLinkProjection", types = {ConnectionLink.class})
+public interface DeaultConnectionLinkProjection {
+    Instant getTimestamp();
+    long getConnectionHash();
+    Agent getSourceAgent();
+    Agent getDestinationAgent();
+    Connection getSourceConnection();
+    Connection getDestinationConnection();
+}
--- src/main/java/com/notrust/server/repository/ConnectionLinkRepository.java
+++ src/main/java/com/notrust/server/repository/ConnectionLinkRepository.java
@@ -1,6 +1,7 @@
 package com.notrust.server.repository;
 
 import com.notrust.server.model.ConnectionLink;
+import com.notrust.server.model.projections.DeaultConnectionLinkProjection;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -13,7 +14,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @SuppressWarnings("unused")
-@RepositoryRestResource(exported = true, path = "connection_links", collectionResourceRel = "connection_links", itemResourceRel = "connection_link")
+@RepositoryRestResource(exported = true, path = "connection_links", collectionResourceRel = "connection_links", itemResourceRel = "connection_link", excerptProjection = DeaultConnectionLinkProjection.class)
 public interface ConnectionLinkRepository extends JpaRepository<ConnectionLink, UUID> {
     @RestResource(exported = true)
     Page<ConnectionLink> findAllBy(Pageable pageable);
