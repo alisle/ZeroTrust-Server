1a2392e6089e7d1ede2c2dfc99edd9c301500cc9
alisle <alex.lisle@gmail.com>
2019-03-07 12:01:22 -0600

    removing node_modules from root

--- node_modules/@swimlane/ngx-charts/release/common/domain.helper.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Based on the data, return an array with unique values.
- *
- * @export
- * @returns array
- * @param results
- */
-export declare function getUniqueXDomainValues(results: any[]): any[];
